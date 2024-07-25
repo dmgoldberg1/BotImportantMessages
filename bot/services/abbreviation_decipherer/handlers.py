5c6cc42451e4d44844d67e452740c1161af5eb1b
MrForquest <folp228@gmail.com>
2024-07-25 15:32:23 +0300

    added function abbr_command_handler for command "abbr"

--- bot/services/abbreviation_decipherer/handlers.py
+++ bot/services/abbreviation_decipherer/handlers.py
@@ -10,9 +10,12 @@ from abbreviation_decipherer.filters import RandomFilter
 abbreviation_decipherer_router = Router()
 
 
-@abbreviation_decipherer_router.message(Command("abbr"))
 @abbreviation_decipherer_router.message(RandomFilter(chance=0.04))
-async def message_handler(msg: Message):
+async def abbr_message_handler(msg: Message):
+    """
+    обрабатывает сообщение с некоторой вероятностью сообщения,
+    из строки берётся одно случайное слово и ему даётся расшировка
+    """
     msg_text = msg.text.lower()
     msg_text = re.sub("[^\w]", " ", msg_text).strip()
     words = set(msg_text.split())
@@ -36,7 +39,43 @@ async def message_handler(msg: Message):
     corr_abbr = random_abbr[2]
 
     if is_orig == 1:
-        text = f'Пиши полностью! НЕ <b>"{orig_abbr}"</b>, a <b>"{random_decipher}"</b>'
+        text = f'Пиши НЕ <b>"{orig_abbr}"</b>, a <b>"{random_decipher}"</b>'
+    elif is_orig == 0:
+        text = f'Тебе так лень написать <b>"{random_decipher}"</b> вместо <b>"{orig_abbr}"</b>?!'
+    else:
+        text = "Позовите Серёгу, мне плохо от ваших сокращений."
+    await msg.reply(text)
+
+
+@abbreviation_decipherer_router.message(Command("abbr"))
+async def abbr_command_handler(msg: Message):
+    """
+    обрабатывает команду abbr, которая имеет аргумент строку
+    из строки берётся одно случайное слово и ему даётся расшировка
+    (подразумевается, что будет передаваться только одно слово)
+    """
+    msg_text = msg.text.lower()
+    msg_text = re.sub("[^\w]", " ", msg_text).strip()
+    words = set(msg_text.split())
+    if not words:
+        return
+    variants = list()
+    for w in words:
+        norm_word = w.strip()
+        is_orig, corr_abbr, deciphers = get_deciphers(norm_word)
+        if deciphers:
+            variants.append((w, is_orig, corr_abbr, deciphers))
+    if not variants:
+        return
+
+    random_abbr = random.choice(variants)
+    random_decipher = random.choice(random_abbr[3])
+    orig_abbr = random_abbr[0]
+    is_orig = random_abbr[1]
+    corr_abbr = random_abbr[2]
+
+    if is_orig == 1:
+        text = f'Пиши НЕ <b>"{orig_abbr}"</b>, a <b>"{random_decipher}"</b>'
     elif is_orig == 0:
         text = f'Тебе так лень написать <b>"{random_decipher}"</b> вместо <b>"{orig_abbr}"</b>?!'
     else:
